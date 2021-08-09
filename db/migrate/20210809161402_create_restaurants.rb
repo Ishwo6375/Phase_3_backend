3eace62b0604325ad4256fef43a7ea5ac3767972
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-08-09 12:15:27 -0400

    created table using migration

---
+++ db/migrate/20210809161402_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[5.2]
+  def change
+   create_table :restaurants do |t|
+    t.string :name
+    t.string :image
+    t.string :restaurant_type
+    t.string :location
+    t.integer :contact
+   end
+  end
+end
